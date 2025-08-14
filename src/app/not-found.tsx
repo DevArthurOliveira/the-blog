6d8c1aa3d0462a0ecb3a1740175add4174ecbfae
Arthur Felipe <arthur_feelipe@hotmail.com>
2025-08-14 18:47:49 -0300

    Substituindo e modificando elementos HTML no React MarkDown e criacao do componente ErrorMessage

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,11 @@
-import clsx from 'clsx';
+import ErrorMessage from '@/components/ErrorMessage';
 
 export default function NotFoundPage() {
   return (
-    <div
-      className={clsx(
-        'min-h-[320px] bg-slate-900 text-slate-100',
-        'mb-16 p-8 rounded-xl',
-        'flex items-center justify-center',
-        'text-center',
-      )}
-    >
-      <div className='flex flex-col justify-center items-center '>
-        <h1 className='font-bold text-4xl p-2'>404</h1>
-        <p className='text-[20px]'>Essa página não existe neste site.</p>
-      </div>
-    </div>
+    <ErrorMessage
+      pageTitle='Página não encontrada'
+      contentTitle='404'
+      content='Essa página não existe neste site.'
+    />
   );
 }
