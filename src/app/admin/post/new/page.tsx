dd6f9023403de0105eb5d99403c9e421907ba720
Arthur Felipe <arthur_feelipe@hotmail.com>
2025-09-02 19:37:34 -0300

    Usando TypeScript para adicionar variações de estilo no componente

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,5 +1,19 @@
+import { Button } from '@/components/Button';
+
 export const dynamic = 'force-static';
 
 export default async function AdminPostNewPage() {
-  return <div className='py-16 text-6xl'>AdminPOstNewPage</div>;
+  return (
+    <div className='py-16 text-6xl'>
+      <Button variant='default' size='sm'>
+        Confirm
+      </Button>
+      <Button variant='ghost' size='md'>
+        Delete
+      </Button>
+      <Button variant='danger' size='lg'>
+        Danger
+      </Button>
+    </div>
+  );
 }
