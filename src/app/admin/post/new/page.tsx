029f96cebd1d2a6c07dc8f3f9b64b40a26f64e42
Arthur Felipe <arthur_feelipe@hotmail.com>
2025-09-03 18:18:17 -0300

    Criando o componente InputText para o formulário

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,33 +1,21 @@
 import { Button } from '@/components/Button';
+import { InputText } from '@/components/InputText';
 import { BugIcon } from 'lucide-react';
 
 export const dynamic = 'force-static';
 
 export default async function AdminPostNewPage() {
   return (
-    <div>
-      <div className='py-16 flex gap-4 items-center'>
-        <Button variant='default' size='sm'>
-          <BugIcon /> Confirm
-        </Button>
-        <Button variant='ghost' size='md'>
-          <BugIcon /> Delete
-        </Button>
-        <Button variant='danger' size='lg'>
-          <BugIcon /> Danger
-        </Button>
+    <div className='flex flex-col gap-6'>
+      <div>
+        <InputText labelText='Nome' placeholder='Digite seu nome' />
       </div>
-
-      <div className='py-16 flex gap-4 items-center'>
-        <Button variant='default' size='sm' disabled>
-          <BugIcon /> Confirm
-        </Button>
-        <Button variant='ghost' size='md' disabled>
-          <BugIcon /> Delete
-        </Button>
-        <Button variant='danger' size='lg' disabled>
-          <BugIcon /> Danger
-        </Button>
+      <div>
+        <InputText
+          disabled
+          labelText='Sobrenome'
+          placeholder='Digite seu sorenome'
+        />
       </div>
     </div>
   );
