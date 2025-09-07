1f063bcafeb8193c3e084407910a6e9b0fd5e98f
Arthur Felipe <arthur_feelipe@hotmail.com>
2025-09-07 11:44:36 -0300

    Validando o arquivo de imagem enviado ao servidor

--- src/actions/revalidate-example.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { revalidateTag } from 'next/cache';
-
-export async function revalidateExampleAction() {
-  console.log('Estou em uma server action');
-
-  revalidateTag('posts');
-}
--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -1,13 +1,16 @@
 'use client';
 
+import { uploadImageAction } from '@/actions/upload/upload-image-action';
 import { Button } from '@/components/Button';
 import { IMAGE_UPLOAD_MAX_SIZE } from '@/lib/constantes';
 import { ImageUp } from 'lucide-react';
-import { useRef } from 'react';
+import { useRef, useTransition } from 'react';
 import { toast } from 'react-toastify';
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isUpload, startTransition] = useTransition();
+
   function handleChooseFile() {
     if (!fileInputRef.current) return;
 
@@ -15,6 +18,7 @@ export function ImageUploader() {
   }
 
   function handleChange() {
+    toast.dismiss();
     if (!fileInputRef) return;
 
     const fileInput = fileInputRef.current;
@@ -25,7 +29,6 @@ export function ImageUploader() {
     if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
       const readableMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024;
       toast.error(`Imagem muito grande. Máx.:${readableMaxSize}KB.`);
-
       fileInput.value = '';
       return;
     }
@@ -33,14 +36,28 @@ export function ImageUploader() {
     const formData = new FormData();
     formData.append('file', file);
 
+    startTransition(async () => {
+      const result = await uploadImageAction(formData);
+
+      if (result.error) {
+        toast.error(result.error);
+        fileInput.value = '';
+        return;
+      }
+
+      toast.success(result.url);
+    });
+
     fileInput.value = '';
   }
+
   return (
     <div className='flex flex-col  gap-2 py-4'>
       <Button onClick={handleChooseFile} type='button' className='self-start'>
         <ImageUp /> Enviar uma imagem
       </Button>
       <input
+        onChange={handleChange}
         ref={fileInputRef}
         className='hidden'
         type='file'
