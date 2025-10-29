f36ede59d8815adfe278b9619b46b0c3d809faa2
Arthur Felipe <arthur_feelipe@hotmail.com>
2025-10-29 17:54:42 -0300

    Gerando e comparando hash de senha com Bcrypt para segurança

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -10,8 +10,21 @@ type LoginActionState = {
 export async function loginAction(state: LoginActionState, formData: FormData) {
   await asyncDelay(3000);
 
+  if (!(formData instanceof FormData)) {
+    return {
+      username: '',
+      error: 'Dados inválidos',
+    };
+  }
+
+  const username = formData.get('username')?.toString() || '';
+  const password = formData.get('passwrod')?.toString() || '';
+
+  const isUsernameValid = username === process.env.LOGIN_USER;
+  const isPasswordValid = password === process.env.LOGIN_PASS;
+
   return {
     username: '',
-    error: 'Teste de erro',
+    error: '',
   };
 }
---
+++ src/utils/generate-hashed-password.ts
@@ -0,0 +1,8 @@
+//import { hashPassword } from '@/lib/login/manage-login';
+
+//(async () => {
+//  const minhaSenha = '';
+// const hashDaSenha = await hashPassword(minhaSenha);
+
+//console.log(hashDaSenha);
+//})();
